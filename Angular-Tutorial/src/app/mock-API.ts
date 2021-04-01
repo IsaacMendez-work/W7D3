29371345ffefd59a76869f61f7332f95df56ab2c
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-04-01 09:49:52 -0400

    notes

--- Angular-Tutorial/src/app/mock-API.ts
+++ Angular-Tutorial/src/app/mock-API.ts
@@ -1,12 +1,12 @@
 import { HeroInterface } from './hero-interface';
 
 export const HEROES: HeroInterface[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
+  { id: 11, name: 'These are' },
+  { id: 12, name: 'ids and names' },
+  { id: 13, name: 'imported from' },
+  { id: 14, name: 'our mock-API.ts' },
+  { id: 15, name: 'file.' },
+  { id: 16, name: 'This information' },
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
