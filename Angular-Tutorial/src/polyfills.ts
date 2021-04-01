1de344c6192d7024f7f57404c0812a8c2a006121
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-04-01 08:03:07 -0400

    Fixed minor typoes

--- Angular-Tutorial/src/polyfills.ts
+++ Angular-Tutorial/src/polyfills.ts
@@ -4,17 +4,12 @@
  *
  * This file is divided into 2 sections:
  *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main file.
  *
- * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera), Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
  * Learn more in https://angular.io/guide/browser-support
- */
-
-/***************************************************************************************************
+ *
  * BROWSER POLYFILLS
  */
 
@@ -23,14 +18,14 @@
  */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/**
+/*
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
-/**
+/*
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  * because those flags need to be set before `zone.js` being loaded, and webpack
@@ -52,14 +47,9 @@
  *
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
- */
-
-/***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-/***************************************************************************************************
+/*
  * APPLICATION IMPORTS
  */
